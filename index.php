7122c0cd6cbb728b271b618b85bbf8e1da972d6c
gi47huwi <andreas.w.wagner@fau.de>
2022-01-22 15:00:20 +0100

    UserIdentification

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 ?>
 
 
-     
+<form>
+    <input type="text" name = "vorname" placeholder="Vorname"><br>
+    <input type="text" name = "nachname" placeholder="Nachname"><br>
+    <input type="email" name = "e-mail" placeholder="E-Mail Adresse"><br>
+    <input type="password" name = "pwd" placeholder="Passwort"><br>
+    <input type="password" name = "pwd-repeat" placeholder="Passwort wiederholen"><br>
+</form>
 
 
 <?php
